e177d75274f6372e0a747605f6db07bd121cd5d8
EC2 Default User <ec2-user@ip-172-31-38-198.us-east-2.compute.internal>
2018-09-11 12:20:31 +0000

    working on a create-user function

--- dynamoFunctions.py
+++ dynamoFunctions.py
@@ -4,17 +4,11 @@ from boto3.dynamodb.conditions import Key, Attr
 import logging.config
 import os
 
-#dir_path=os.path.dirname(os.path.realpath(__file__))
-#logConfigPath=os.path.join(dir_path, 'config', 'logconfig.json')
-#
-#logger=logging.getLogger(__name__)
-##logger.setLevel(logging.INFO)
-#setupLogging(logConfigPath)
 
 def getPlayer(table, sport, id):
   result=table.query(KeyConditionExpression=Key('sport').eq(sport) & Key('id').eq(id))
   if result['Count'] > 0:
-    return result['Items']
+    return result['Items'][0]
   else:
     logger.info('could not find user with sport %s and id %s' % (sport, id))
     return None
@@ -35,10 +29,14 @@ def getSportsPlayers(table, sport):
   playerCount+=response['Count']
   return sportsPlayers, playerCount
 
-def createPlayer(table, playerDict):
+def createPlayer(client, tableName, playerDict):
   if 'id' not in playerDict or 'sport' not in playerDict:
     logger.error("could not create player.  id or sport not in playerDictionary for object: \n %d" % playerDict)
-  if(getPlayer(table, 
+  else:
+    result=client.put_item(TableName=tableName, Item=playerDict,
+                    ConditionExpression='attribute_not_exists(id)')
+    logger.info('creation result is %s' % result)
+    #todo: need to log if a modification occured.
 
 def setupLogging(logConfigPath, defaultLevel=logging.INFO):
   if(not os.path.isfile(logConfigPath)):
@@ -49,6 +47,7 @@ def setupLogging(logConfigPath, defaultLevel=logging.INFO):
 
 def main():
   dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+  dynamoClient=boto3.client('dynamodb')
   playerTable = dynamodb.Table('Players')
   global logger
   dir_path=os.path.dirname(os.path.realpath(__file__))
@@ -60,8 +59,11 @@ def main():
  
 #  players, count=getSportsPlayers(playerTable, 'baseball'
 #  print(players)
-  player=getPlayer(playerTable, 'baseball', '5856262312')
+  player=getPlayer(playerTable, 'baseball', '585626')
+  player['firstname']='Rickyyy'
+  player['id']='585626254'
+  createPlayer(client=dynamoClient, tableName='Players', playerDict=player)
+  player=getPlayer(playerTable, 'baseball', '585626254')
   print(player)
-
 if __name__ == '__main__':
   main()
