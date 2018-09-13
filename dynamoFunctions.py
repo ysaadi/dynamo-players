339b7a76f6528097f519e3ad15fc5f0e5432b597
EC2 Default User <ec2-user@ip-172-31-38-198.us-east-2.compute.internal>
2018-09-13 23:37:35 +0000

    modified create-users.  added functions to create sports/delete sports.  added modify/delete functions to the user

--- dynamoFunctions.py
+++ dynamoFunctions.py
@@ -29,14 +29,57 @@ def getSportsPlayers(table, sport):
   playerCount+=response['Count']
   return sportsPlayers, playerCount
 
-def createPlayer(client, tableName, playerDict):
-  if 'id' not in playerDict or 'sport' not in playerDict:
-    logger.error("could not create player.  id or sport not in playerDictionary for object: \n %d" % playerDict)
-  else:
-    result=client.put_item(TableName=tableName, Item=playerDict,
+def createPlayer(playerTable, sportTable, playerDict):
+  dynamoClient=boto3.client('dynamodb')
+  if (getSport(table=sportTable, sport=playerDict['sport'])==None):
+    logger.error('user {userid} has an invalid sport: {sport}.  Please correct
+                 the sport name or add the sport the sports
+                 table.'.format(userid=playerDict['id'],
+                                sport=playerDict['sport']))
+    return
+  try:
+    result=playerTable.put_item(Item=playerDict,
                     ConditionExpression='attribute_not_exists(id)')
     logger.info('creation result is %s' % result)
     #todo: need to log if a modification occured.
+  except dynamoClient.exceptions.ConditionalCheckFailedException as err:
+    logger.error("Creation Failed.  User already exists: {0}".format(err))
+
+def deletePlayer(table, sport, id):
+  result=table.delete_item(Key={'id': id, 'sport': sport})
+  logger.info('result of deletion is : {0}'.format(result))
+
+def updatePlayer(table,playerDict):
+  dynamoClient=boto3.client('dynamodb')
+  try:
+    result=table.put_item(Item=playerDict,
+                    ConditionExpression='attribute_exists(id)')
+    logger.info('creation result is %s' % result)
+    #todo: need to log if a modification occured.
+  except dynamoClient.exceptions.ConditionalCheckFailedException as err:
+    logger.error("Update Failed.  User does not exist: {0}".format(err))
+
+def createSport(table,sportDict):
+  dynamoClient=boto3.client('dynamodb')
+  try:
+    result=table.put_item(Item=sportDict,
+                    ConditionExpression='attribute_not_exists(sport)')
+    logger.info('creation result is %s' % result)
+    #todo: need to log if a modification occured.
+  except dynamoClient.exceptions.ConditionalCheckFailedException as err:
+    logger.error("Creation Failed.  User already exists: {0}".format(err))
+
+def getSport(table, sport):
+  result=table.query(KeyConditionExpression=Key('sport').eq(sport))
+  if result['Count'] > 0:
+    return result['Items'][0]
+  else:
+    logger.info('could not find sport %s' % (sport))
+    return None
+
+def deleteSport(sport, id):
+  result=table.delete_item(Key={ 'sport': sport})
+  logger.info('result of deletion is : {0}'.format(result))
 
 def setupLogging(logConfigPath, defaultLevel=logging.INFO):
   if(not os.path.isfile(logConfigPath)):
@@ -44,6 +87,7 @@ def setupLogging(logConfigPath, defaultLevel=logging.INFO):
   else:
     with open(logConfigPath) as logConfigFile:
       logging.config.dictConfig(json.load(logConfigFile))
+
 
 def main():
   dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
@@ -55,15 +99,20 @@ def main():
   logger=logging.getLogger(__name__)
   #logger.setLevel(logging.INFO)
   setupLogging(logConfigPath)
-
- 
+  
+  sportTable=dynamodb.Table('Sports')
+  baseball={'sport':'baseball'}
+  football={'sport':'football'}
+  basketball={'sport':'basketball'}
+  createSport(table=sportTable, sportDict=football)
+  createSport(table=sportTable, sportDict=basketball)
 #  players, count=getSportsPlayers(playerTable, 'baseball'
 #  print(players)
-  player=getPlayer(playerTable, 'baseball', '585626')
-  player['firstname']='Rickyyy'
-  player['id']='585626254'
-  createPlayer(client=dynamoClient, tableName='Players', playerDict=player)
-  player=getPlayer(playerTable, 'baseball', '585626254')
-  print(player)
+#  player=getPlayer(playerTable, 'baseball', '585626')
+#  player['firstname']='Rick'
+#  player['id']='5856262548'
+#  deletePlayer(playerTable, player['sport'], player['id'])
+#  player=getPlayer(playerTable, 'baseball', '5856262548')
+#  print(player)
 if __name__ == '__main__':
   main()
